352da9d5378077c22ae6734b60de643908e53121
Ishan <ishanchowdhur@gmail.com>
2024-01-22 15:49:14 -0800

    Add id transformation to data_transforms.py

--- etl/data_transforms.py
+++ etl/data_transforms.py
@@ -1,6 +1,12 @@
 from datetime import datetime
 
 
+def transform_id(input_str: str) :
+    if input_str:
+        return input_str.split("_")[1]
+    return None
+
+
 def transform_date(input_str: str) -> datetime:
     return datetime.utcfromtimestamp(int(input_str))
 
@@ -12,7 +18,9 @@ def transform_date_with_bool(input_str: str):
 
 
 def transform_field(field_name, data):
-    if field_name in ["created_utc", "retrieved_on"]:
+    if field_name in ["parent_id", "link_id", "subreddit_id"]:
+        return transform_id(data)
+    elif field_name in ["created_utc", "retrieved_on"]:
         return transform_date(data)
     elif field_name == "edited":
         return transform_date_with_bool(data)
